87f0a11be5d8447bb8b714fcb597a8dec0f4c333
cesarhdzf <hernandez.feregrino.cesar.arturo@gmail.com>
2024-09-23 19:26:01 -0600

    Primer commit

---
+++ script.js
@@ -0,0 +1,32 @@
+//18 09 2024 /Hernandez Fergrino Cesar Arturo
+
+/*
+comentario
+en varias
+lineas
+*/
+
+//Variables
+
+let nombre ="Arturo";
+const PI = 3.1416;
+var edad = 18;
+
+/*
+Cadena de texto (string): "Hola"
+Numero (number): 5,12,53
+Booleanos (boolean): true, false
+*/
+
+//Ejemplo
+let mensaje = "Hola mundo";
+let numero = 42;
+let Estudiante = true;
+
+//Operadores matematicos
+let a = 5;
+let b = 2;
+let suma = a+b;//7
+let resta = a-b;//3
+let multiplicacion = a*b;//10
+let division = a/b;//2.5
