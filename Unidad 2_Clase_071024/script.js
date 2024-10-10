3c6d5ef9229421aa05f01115d822b53ea7f63537
cesarhdzf1 <hernandez.feregrino.cesar.arturo@gmail.com>
2024-10-09 20:45:22 -0600

    Comentar codigo js

--- Unidad 2_Clase_071024/script.js
+++ Unidad 2_Clase_071024/script.js
@@ -1,16 +1,20 @@
+//Pone el valor escrito en la pantalla
 function appendToDisplay(value) {
     document.getElementById('display').value += value;
 }
 
+//Limpia la pantalla de valores
 function clearDisplay() {
     document.getElementById('display').value = "";
 }
 
+//Borra un digito del valor que este en la pantalla
 function deleteLast(){
     let displayValue = document.getElementById('display').value;
     document.getElementById('display').value = displayValue.slice(0, -1);
 }
 
+//Calcula las operaciones indicadas
 function calculateResult(){
     try {
         let result = eval(document.getElementById('display').value);
